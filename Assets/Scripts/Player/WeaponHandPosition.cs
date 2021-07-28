af95f47a8c0431c5b67def08b8af80ff06ae9f3a
nbergstrand <bergstrand.niklas@gmail.com>
2021-07-28 11:01:44 +0100

    Added gun and rigged the hands

---
+++ Assets/Scripts/Player/WeaponHandPosition.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponHandPosition : MonoBehaviour
+{
+    [SerializeField]
+    Transform _handTransform;
+
+    [SerializeField]
+    float _lerpSpeed;
+
+
+    void LateUpdate()
+    {
+
+        Vector3 targetPosition = _handTransform.position;
+        transform.position = Vector3.Lerp(transform.position, targetPosition, _lerpSpeed * Time.deltaTime);
+        transform.rotation = Quaternion.Euler(_handTransform.rotation.eulerAngles);
+
+    }
+}
